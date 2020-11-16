73547434f6ce230b71193f0661739af5af724229
Joshua Rotenberg <joshua.rotenberg@gmail.com>
2020-11-16 12:56:52 -0500

    loading wines on index from firebase successful

--- src/controllers/wineIndexController.js
+++ src/controllers/wineIndexController.js
@@ -5,46 +5,6 @@ import database from "../firebaseConfig";
 
 export default function wineIndexController() {
 
-
-database
-.ref("wines")
-.on("value", (results) => {
-    results.forEach((result) => {
-      const wine = result.val();
-      const wineId = result.key;
-    });
-});
-
-document
-.getElementById("add-wine-form")
-.addEventListener("submit", function(event) {
-    event.preventDefault();
-    console.log("form submitted");
-
-
-
-    const newWine = {
-        name: document.getElementById("name").value,
-        year: document.getElementById("year").value,
-        grapes: document.getElementById("grapes").value,
-        country: document.getElementById("country").value,
-        region: document.getElementById("region").value,
-        price: document.getElementById("price").value,
-        picture: document.getElementById("picture").value,
-        description: document.getElementById("description").value
-    }
-    
-    //add record to Firebase using the Firebase SDK
-
-    console.log(newWine);
-
-
-    database
-    .ref("wines")
-    .push(newWine);
-
-});
-
     axios
     .get("templates/wineIndex.hbs")
     .then((wineIndexResponse) => {
@@ -61,9 +21,61 @@ document
     
         document
         .getElementById("root")
-        .innerHTML = wineIndexTemplateFunc();
+        .innerHTML = windIndexTemplateFunc();
 
 
-        const wines = document.getElementById("wine-index");
+
+        database
+        .ref("wines")
+        .on("value", (results) => {
+            results.forEach((result) => {
+
+                const wines = document.getElementById("wine-index");
+                const data = result.val();
+                    
+                
+                wines.innerHTML += wineResultTemplateFunc({
+                    name: data.name,
+                    year: data.year,
+                    grapes: data.grapes,
+                    country: data.country,
+                    region: data.region,
+                    price: data.price,
+                    picture: data.picture,
+                    description: data.description             
+                });
+
+                console.log(data);
+                    
+            });
+        });
+
+        document
+        .getElementById("add-wine-form")
+        .addEventListener("submit", function(event) {
+            event.preventDefault();
+            console.log("form submitted");
+
+            const newWine = {
+                name: document.getElementById("name").value,
+                year: document.getElementById("year").value,
+                grapes: document.getElementById("grapes").value,
+                country: document.getElementById("country").value,
+                region: document.getElementById("region").value,
+                price: document.getElementById("price").value,
+                picture: document.getElementById("picture").value,
+                description: document.getElementById("description").value
+            }
+            
+            //add record to Firebase using the Firebase SDK
+
+            console.log(newWine);
+
+
+            database
+            .ref("wines")
+            .push(newWine);
+
+        });
     }
 }
